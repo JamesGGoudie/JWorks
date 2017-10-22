4f7092795508b95ae92db9d81687229ae4544f35
chiusin97525 <sinchi.chiu97525@gmail.com>
2017-10-22 13:36:01 -0400

    Implemented main method in JWorks

--- JWorks/src/driver/JWorks.java
+++ JWorks/src/driver/JWorks.java
@@ -1,8 +1,29 @@
 package driver;
 
+import io.UI;
 
 public class JWorks {
-  public static void main(String[] args) {
 
+  private static UI ui = new UI();
+  private static Interpreter interpreter = new Interpreter();
+  private static UIParser parser = new UIParser();
+
+  private static boolean exit = false;
+  private static String input;
+  private static String[][] formatted_input;
+
+
+  public static void main(String[] args) {
+    // Keep the program running
+    while (!exit) {
+      input = ui.userChoicePrompt();
+
+      if (input.equals("exit")) {
+        exit = true;
+      } else {
+         formatted_input = parser.parseInput(input);
+         interpreter.executeAction(formatted_input);
+      }
+    }
   }
 }
