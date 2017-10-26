47e75dcadde0b1ad08085884634d21e49ec17f12
James Gabriel Goudie <james.goudie@mail.utoronto.ca>
2017-10-26 12:38:46 -0400

    Added functionality to delete a problem set from the database as well as the respective column in attemptsRemaining.

--- JWorks/src/database/DatabaseAlterer.java
+++ JWorks/src/database/DatabaseAlterer.java
@@ -20,6 +20,7 @@ public class DatabaseAlterer {
     String sql = "ALTER TABLE ATTEMPTSREMAINING ADD COLUMN "
         + columnName
         + ";";
+    
     boolean result = false;
     
     try {
@@ -30,6 +31,37 @@ public class DatabaseAlterer {
       result = true;
     } catch (SQLException e) {
       System.out.println("Could not add a problem set column to the attemptsRemaining table.");
+      e.printStackTrace();
+    }
+    
+    return result;
+  }
+  
+  /**
+   * Removes a problem set's column from the AttemptsRemaining Table
+   * @param problemSetKey The unique ID of the problem set.
+   * @param connection The connection to the database file.
+   * @return True if the column was removed, false if an error occurred.
+   */
+  protected static boolean removeProblemSetFromAttemptsRemaining(int problemSetKey,
+      Connection connection) {
+    
+    String columnName = "PROBLEMSET" + problemSetKey;
+    
+    String sql = "ALTER TABLE ATTEMPTSREMAINING DROP COLUMN "
+        + columnName
+        + ";";
+    
+    boolean result = false;
+    
+    try {
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      
+      preparedStatement.executeUpdate();
+      
+      result = true;
+    } catch (SQLException e) {
+      System.out.println("Could not remove a problem set column from attemptsRemaining.");
       e.printStackTrace();
     }
     
--- JWorks/src/database/DatabaseDeleter.java
+++ JWorks/src/database/DatabaseDeleter.java
@@ -9,7 +9,7 @@ public class DatabaseDeleter {
   /**
    * Removes a problem from the database.
    * @param problemKey The unique key of the problem.
-   * @param connection The connection to the database.
+   * @param connection The connection to the database file.
    * @return True if the problem was deleted, false otherwise.
    */
   protected static boolean deleteProblem(int problemKey, Connection connection) {
@@ -23,7 +23,31 @@ public class DatabaseDeleter {
       result = true;
     } catch (SQLException e) {
       e.printStackTrace();
-      System.out.println("A problem occurred while attempting to delete a question.");
+      System.out.println("A problem occurred while attempting to delete a problem.");
+    }
+    
+    return result;
+  }
+  
+  /**
+   * Removes a problem set from the database along with the attempts remaining for each student.
+   * @param problemSetKey The unique ID of the problem set.
+   * @param connection The connection to the database file.
+   * @return True if all components of the problem set was deleted, false otherwise.
+   */
+  protected static boolean deleteProblemSet(int problemSetKey, Connection connection) {
+    String sql = "DELETE FROM PROBLEMSETS WHERE ID = ?";
+    boolean result = false;
+    
+    try {
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setInt(1, problemSetKey);
+      preparedStatement.executeUpdate();
+      
+      result = DatabaseAlterer.removeProblemSetFromAttemptsRemaining(problemSetKey, connection);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.println("A problem occurred while attempting to delete a problem set.");
     }
     
     return result;
