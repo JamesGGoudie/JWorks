0901efb53295d13cc3db54a60b7475a114eac051
James Gabriel Goudie <james.goudie@mail.utoronto.ca>
2017-10-26 11:59:31 -0400

    Added functionality to update the name, email, and attempts remaining for a problem set of a student and to update the max attempts allowed for a problem set and the problems in the set.

--- JWorks/src/database/DatabaseUpdater.java
+++ JWorks/src/database/DatabaseUpdater.java
@@ -21,9 +21,10 @@ public class DatabaseUpdater {
       PreparedStatement preparedStatement = connection.prepareStatement(sql);
       preparedStatement.setString(1, newQuestion);
       preparedStatement.setInt(2, problemKey);
+      
       preparedStatement.executeUpdate();
+      
       result = true;
-        
     } catch (SQLException e) {
       System.out.println("A problem occurred while attempting to modify a problems question.");
       e.printStackTrace();
@@ -47,11 +48,165 @@ public class DatabaseUpdater {
       PreparedStatement preparedStatement = connection.prepareStatement(sql);
       preparedStatement.setString(1, newAnswer);
       preparedStatement.setInt(2, problemKey);
+      
       preparedStatement.executeUpdate();
+      
       result = true;
-        
     } catch (SQLException e) {
       System.out.println("A problem occurred while attempting to modify a problems answer.");
+      e.printStackTrace();
+    }
+    
+    return result;
+  }
+  
+  /**
+   * Changes the specified student's name to the one given.
+   * @param studentNumber The unique ID of the student.
+   * @param name The new name of the student.
+   * @param connection The connection to the database file.
+   * @return True if the name was changed, false otherwise.
+   */
+  protected static boolean updateStudentName(int studentNumber, String name, Connection connection) {
+    String sql = "UPDATE STUDENTS SET NAME = ? WHERE STUDENTNUMBER = ?;";
+    boolean result = false;
+    
+    try {
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setString(1, name);
+      preparedStatement.setInt(2, studentNumber);
+      
+      preparedStatement.executeUpdate();
+      
+      result = true;
+    } catch (SQLException e) {
+      System.out.println("A problem occurred while attempting to change a students name.");
+      e.printStackTrace();
+    }
+    
+    return result;
+  }
+  
+  /**
+   * Changes the specified student's email to the one given.
+   * @param studentNumber The unique ID of the student.
+   * @param email The new email of the student.
+   * @param connection The connection to the database file.
+   * @return True if the email was changed, false otherwise.
+   */
+  protected static boolean updateStudentEmail(int studentNumber, String email,
+      Connection connection) {
+    String sql = "UPDATE STUDENTS SET EMAIL = ? WHERE STUDENTNUMBER = ?;";
+    boolean result = false;
+    
+    try {
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setString(1, email);
+      preparedStatement.setInt(2, studentNumber);
+      
+      preparedStatement.executeUpdate();
+      
+      result = true;
+    } catch (SQLException e) {
+      System.out.println("A problem occurred while attempting to change a students email.");
+      e.printStackTrace();
+    }
+    
+    return result;
+  }
+  
+  /**
+   * Changes the max amount of attempts allowed for the given problem set.
+   * @param problemSetKey The unique ID of the problem set.
+   * @param maxAttempts The maximum amount of allowed attempts.
+   * @param connection The connection to the database file.
+   * @return True if the attempts cap was changed, false otherwise.
+   */
+  protected static boolean updateProblemSetMaxAttempts(int problemSetKey, int maxAttempts,
+      Connection connection) {
+    String sql = "UPDATE PROBLEMSETS SET MAXATTEMPTS = ? WHERE ID = ?;";
+    boolean result = false;
+    
+    try {
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setInt(1, maxAttempts);
+      preparedStatement.setInt(2, problemSetKey);
+      
+      preparedStatement.executeUpdate();
+      
+      result = true;
+    } catch (SQLException e) {
+      System.out.println("A problem occurred while updating the max amount of attempts.");
+      e.printStackTrace();
+    }
+    
+    return result;
+  }
+
+  /**
+   * Replaces the problems in the desired problem set with those given.
+   * @param problemSetKey The unique ID of the problem set.
+   * @param newProblems The unique IDs of the problems.
+   * @param connection The connection to the database file.
+   * @return True if the problem set was modified, false otherwise.
+   */
+  protected static boolean updateProblemSetProblems(int problemSetKey, int[] newProblems,
+      Connection connection) {
+    
+    String problemsFormatted = "";
+    
+    for (int problemID : newProblems) {
+      problemsFormatted = Integer.toString(problemID) + ";";
+    }
+    
+    String sql = "UPDATE PROBLEMSETS SET PROBLEMS = ? WHERE ID = ?;";
+    boolean result = false;
+    
+    try {
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setString(1, problemsFormatted);
+      preparedStatement.setInt(2, problemSetKey);
+      
+      preparedStatement.executeUpdate();
+      
+      result = true;
+    } catch (SQLException e) {
+      System.out.println("A problem occurred while updating the problems in the set.");
+      e.printStackTrace();
+    }
+    
+    return result;
+  }
+  
+  /**
+   * Changes the amount of remaining attempts available to the given student for the given problem
+   * set.
+   * @param problemSetKey The unique ID of the problem set.
+   * @param studentNumber The unique number of the student.
+   * @param attemptsRemaining The new amount of remaining attempts.
+   * @param connection The connection to the database file.
+   * @return True if the remaining attempts was modified, false otherwise.
+   */
+  protected static boolean updateAttemptsRemaining(int problemSetKey, int studentNumber,
+      int attemptsRemaining, Connection connection) {
+    
+    String columnName = "PROBLEMSET" + problemSetKey;
+    
+    String sql = "UPDATE ATTEMPTSREMAINING SET "
+        + columnName
+        + " = ? WHERE ID = ?;";
+    boolean result = false;
+    
+    try {
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setInt(1, attemptsRemaining);
+      preparedStatement.setInt(2, problemSetKey);
+      
+      preparedStatement.executeUpdate();
+      
+      result = true;
+    } catch (SQLException e) {
+      System.out.println("A problem occurred while updating the problems in the set.");
       e.printStackTrace();
     }
     
