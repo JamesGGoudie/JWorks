c31448ce542307382b111bfc6e8087e9f93396fb
Samuel Zhou <samuel-zhou@outlook.com>
2017-10-29 15:44:33 -0400

    Add basic skeleton for problem set commands

---
+++ JWorks/src/command/AddSimpleProblemSetCommand.java
@@ -0,0 +1,30 @@
+package command;
+
+import action.ActionParameters;
+import action.AddProblemSetAction;
+import problem.SimpleProblemSet;
+
+public class AddSimpleProblemSetCommand implements ICommand {
+    /**
+     * Saves a new Simple Problem Set, given a list of Problem IDs.
+     * @param args the arguments for the command to use. This should be a list of Problem IDs to add the Problem set.
+     * @return whether or not the Problem Set is created
+     */
+    @Override
+    public boolean execute(String[] args) {
+        // Create problem set
+        SimpleProblemSet problemSet = new SimpleProblemSet();
+
+        for (String id : args) {
+            // TODO: Add problems via id
+            // problemSet.addProblem()
+        }
+
+        ActionParameters params = new ActionParameters();
+        params.setProblemSet(problemSet);
+
+        AddProblemSetAction action = new AddProblemSetAction();
+        action.execute(params);
+        return true;
+    }
+}
