0dd489c229e531dbf8a79ed30b4e0d8212e6dac6
Samuel Zhou <samuel-zhou@outlook.com>
2017-10-29 13:01:57 -0400

    Add Problem Set models

---
+++ JWorks/src/problem/ProblemSet.java
@@ -0,0 +1,23 @@
+package problem;
+
+import java.util.List;
+
+public abstract class ProblemSet {
+
+    protected int maxAttempts;
+    protected int id;
+
+    /**
+     * Retrieves a list of Problems that the problem set contains.
+     * @return the Problems in the problem set
+     */
+    public abstract List<Problem> getQuestions();
+
+    /**
+     * Returns the maximum number of attempts for this Problem Set.
+     * @return the maximum number of attempts for this Problem Set. -1 represents no limit.
+     */
+    public int getMaximumNumberOfAttempts() {
+        return maxAttempts;
+    };
+}
---
+++ JWorks/src/problem/SimpleProblemSet.java
@@ -0,0 +1,53 @@
+package problem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class to represent a Problem Set with a determinate set of problems
+ */
+public class SimpleProblemSet extends ProblemSet {
+
+    protected List<Problem> problems;
+
+    /**
+     * Initializes a Problem Set with an empty list of Problems.
+     */
+    public SimpleProblemSet() {
+        this.problems = new ArrayList<>();
+    }
+
+    /**
+     * Initializes a Problem Set with the given list of Problems.
+     * @param problems the list of Problems that the Problem Set contains
+     */
+    public SimpleProblemSet(List<Problem> problems) {
+        this.problems = problems;
+    }
+
+    /**
+     * Retrieves the determinate list of Problems contained in this Problem Set.
+     * @return the list of Problems contained in this Problem Set
+     */
+    @Override
+    public List<Problem> getQuestions() {
+        return problems;
+    }
+
+    /**
+     * Removes the given Problem from the Problem Set
+     * @param problem the Problem to remove from the Problem Set
+     * @return
+     */
+    public boolean removeProblem(Problem problem) {
+        return problems.remove(problem);
+    }
+
+    /**
+     * Adds the specified Problem to the Problem Set
+     * @param problem the Problem to add to the Problem Set
+     */
+    public void addProblem(Problem problem) {
+        problems.add(problem);
+    }
+}
