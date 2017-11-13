92b773870218f416fd8734df5787f92f0dd63b82
Samuel Zhou <samuel-zhou@outlook.com>
2017-11-12 22:21:31 -0500

    Implement problem set command call and error messages from GUI

--- JWorks/src/gui/AddProblemSetScreenController.java
+++ JWorks/src/gui/AddProblemSetScreenController.java
@@ -1,17 +1,19 @@
 package gui;
 
-import javafx.beans.property.Property;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import models.Problem;
+import models.SimpleProblemSet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class AddProblemSetScreenController extends Controller {
@@ -33,6 +35,12 @@ public class AddProblemSetScreenController extends Controller {
 
     @FXML
     private Button saveProblemSetButton;
+
+    @FXML
+    private TextField maxAttemptsField;
+
+    @FXML
+    private Label errorLabel;
 
     @FXML
     private ViewProblemsController problemTableViewController;
@@ -78,6 +86,23 @@ public class AddProblemSetScreenController extends Controller {
             }
         });
 
+        saveProblemSetButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Refuse if any fields are empty
+                if (problemSetNameField.getText().length() == 0
+                        || maxAttemptsField.getText().length() == 0
+                        || releaseDateField.getValue() == null
+                        || dueDateField.getValue() == null) {
+                    errorLabel.setText("Please ensure all fields are filled in.");
+                } else if (problemList.isEmpty()) {
+                    errorLabel.setText("Please select at least 1 problem.");
+                } else {
+                    saveProblemSet();
+                }
+            }
+        });
+
         updateTableList();
     }
 
@@ -105,5 +130,52 @@ public class AddProblemSetScreenController extends Controller {
 
     private void updateTableList() {
         problemListTable.getItems().setAll(problemList);
+    }
+
+    private void saveProblemSet() {
+        // Reset error
+        errorLabel.setText("");
+
+        // Check for valid options
+        String problemSetName = problemSetNameField.getText();
+        String maxAttemptsText = maxAttemptsField.getText();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate;
+        Date endDate;
+        int maxAttempts;
+
+
+        try {
+            startDate = dateFormat.parse(releaseDateField.getValue().format(DateTimeFormatter.ISO_LOCAL_DATE));
+            endDate = dateFormat.parse(dueDateField.getValue().format(DateTimeFormatter.ISO_LOCAL_DATE));
+            maxAttempts = Integer.parseInt(maxAttemptsText);
+        } catch (ParseException e) {
+            errorLabel.setText("Please ensure the dates are entered correctly.");
+            return;
+        } catch (NumberFormatException e) {
+            errorLabel.setText("Please ensure the max attempts field is strictly numeric.");
+            maxAttemptsField.clear();
+            maxAttemptsField.requestFocus();
+            return;
+        }
+
+        if (startDate.after(endDate)) {
+            errorLabel.setText("Release date must be before due date.");
+            return;
+        }
+
+        // Create the problem set object if possible
+        SimpleProblemSet problemSet = new SimpleProblemSet(problemList);
+        problemSet.setStartTime(startDate);
+        problemSet.setEndTime(endDate);
+        problemSet.setMaxAttempts(maxAttempts);
+
+        List<Problem> problems = new ArrayList<>(problemList);
+
+        for (Problem p : problems) {
+            problemSet.addProblem(p);
+        }
+
+        manager.addProblemSet(problemSet);
     }
 }
--- JWorks/src/gui/AddProblemSetScreenManager.java
+++ JWorks/src/gui/AddProblemSetScreenManager.java
@@ -1,6 +1,12 @@
 package gui;
 
 import javafx.scene.layout.Pane;
+import models.Problem;
+import models.ProblemSet;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddProblemSetScreenManager extends Manager {
     /**
@@ -13,5 +19,23 @@ public class AddProblemSetScreenManager extends Manager {
         AddProblemSetScreenController controller =
                 loader.getController();
         controller.start(this);
+    }
+
+    public void addProblemSet(ProblemSet problemSet) {
+        // Create string args array to call the appropriate command from interpreter
+        List<String> argList = new ArrayList<>();
+        argList.add("3"); // add problem set command number
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
+
+        // Start date, end date, max attempts, followed by problem ids
+        argList.add(dateFormat.format(problemSet.getStartTime()));
+        argList.add(dateFormat.format(problemSet.getEndTime()));
+        argList.add(String.valueOf(problemSet.getMaxAttempts()));
+
+        for (Problem p : problemSet.getQuestions()) {
+            argList.add(String.valueOf(p.getId()));
+        }
+
+        interpreter.executeAction(argList.toArray(new String[argList.size()]));
     }
 }
