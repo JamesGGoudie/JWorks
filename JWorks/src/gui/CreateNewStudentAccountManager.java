1f78057d82a080cb4767b8277e0f12d371fa89fa
chiusin97525 <sinchi.chiu97525@gmail.com>
2017-11-18 15:38:42 -0500

    Implemented password fields and linked to backend in create student screen

--- JWorks/src/gui/CreateNewStudentAccountManager.java
+++ JWorks/src/gui/CreateNewStudentAccountManager.java
@@ -2,15 +2,23 @@ package gui;
 
 import javafx.scene.layout.Pane;
 
-public class CreateNewStudentAccountManager extends Manager{
-  private CreateNewStudentAccountController controller;
-  
-  public void showScreen(Pane innerPane) {
-    // load the new inner pane from fxml file
-    loader = loadNewPane(loader, innerPane, "CreateNewStudentAccount.fxml");
-    // Load the controller and start the controller
-    controller = loader.<CreateNewStudentAccountController>getController();
-    controller.start(this);
-  }
+public class CreateNewStudentAccountManager extends Manager {
+	private CreateNewStudentAccountController controller;
+	private final String COMMAND = "AddStudentCommand";
+	private String[] inputValues;
+
+	public void showScreen(Pane innerPane) {
+		// load the new inner pane from fxml file
+		loader = loadNewPane(loader, innerPane, "CreateNewStudentAccount.fxml");
+		// Load the controller and start the controller
+		controller = loader.<CreateNewStudentAccountController>getController();
+		controller.start(this);
+	}
+
+	public void createStudentAccount(String name, String email, String password, String studentNumber) {
+		inputValues = new String[] {COMMAND, name, email, password, studentNumber};
+		interpreter.executeAction(inputValues);
+	}
+	
 
 }
