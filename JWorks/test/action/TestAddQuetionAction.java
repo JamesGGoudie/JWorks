0c040b69eff8165f9913b06bf2cf8e2baa446c3f
Reizmoz <andyy.zhang@mail.utoronto.ca>
2017-11-18 23:38:39 -0500

    tried to mock and created sample tests for AddQuestions and AddStudent

--- JWorks/test/action/TestAddQuetionAction.java
+++ JWorks/test/action/TestAddQuetionAction.java
@@ -2,13 +2,71 @@ package action;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import action.Action;
+import action.AddProblemSetAction;
+import action.AddQuestionAction;
+import action.AddStudentAction;
+import action.LoginAction;
+import action.ViewQuestionAction;
+
+import databaseAPI.DatabaseAPI;
+import databaseAPI.DatabaseStoreAPI;
+import exceptions.DatabaseInsertException;
+import models.Problem;
+import models.SingleAnswerProblem; 
 
 public class TestAddQuetionAction {
+	
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddQuestion() {
+		String[] question = {"Blue","4"};
+		Problem problem = new SingleAnswerProblem(question[0], question[1]);
+		
+		MockDatabaseAPI mockStore = new MockDatabaseStoreAPI(); // Encountered Error for action.excute() when casting
+		
+		DatabaseAPI databaseAPI = new DatabaseStoreAPI();
+		
+		// Testing this way to see if return the Problem object,
+		// confused about mocking in this part
+		Action action = new AddQuestionAction();
+		
+		Object actual = action.execute(problem, databaseAPI);
+		Problem expected = problem;
+		
+		assertEquals(expected, actual);
+
+	}
+	
+	@Test
+	public void testAddNULLQuestion() {
+		String[] question = {null, null};
+		Problem problem = new SingleAnswerProblem(question[0], question[1]);
+		
+		DatabaseAPI databaseAPI = new DatabaseStoreAPI();
+		
+		Action action = new AddQuestionAction();
+		
+		Object actual = action.execute(problem, databaseAPI);
+		Problem expected = problem;
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testAddQuestionExpectError() {
+		// how to break this
+		
+		//exception.expect(DatabaseInsertException.class);
+		assertTrue(true);
 	}
 
 }
--- JWorks/test/action/TestAddStudentAction.java
+++ JWorks/test/action/TestAddStudentAction.java
@@ -2,13 +2,42 @@ package action;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import action.Action;
+import action.AddStudentAction;
+
+import databaseAPI.DatabaseAPI;
+import databaseAPI.DatabaseStoreAPI;
+
+import models.Student;
 
 public class TestAddStudentAction {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddStudent() {
+		Student student = new Student("Bob", "bob@email.com", "000000", 1);
+		
+		DatabaseAPI databaseAPI = new DatabaseStoreAPI();
+		Action action = new AddStudentAction();
+		
+		Object actual = action.execute(student, databaseAPI);
+		Object expected = true;
+		
+		assertEquals( "FAILED TO ADD STUDENT", expected, actual);
+	}
+	
+	@Test
+	public void testAddStudentWithNullProperties() {
+		
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testAddStudentExpectError() {
+		
+		assertTrue(true);
 	}
 
 }
