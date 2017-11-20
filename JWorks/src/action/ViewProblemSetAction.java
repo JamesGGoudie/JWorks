7a3090c41b0719a091c3fc3d9840ae41e7c32a44
Samuel Zhou <samuel-zhou@outlook.com>
2017-11-19 21:44:27 -0500

    Implement login logic for students and instructors

---
+++ JWorks/src/action/ViewProblemSetAction.java
@@ -0,0 +1,15 @@
+package action;
+
+public class ViewProblemSetAction extends Action {
+    /**
+     * Executes this Action to return a list of all problem sets.
+     *
+     * @param params The parameters to pass into the Action.
+     *               The first parameter is the DatabaseExtractAPI object.
+     * @return the output of the action. This
+     */
+    @Override
+    public Object execute(Object... params) {
+        return null;
+    }
+}
---
+++ JWorks/src/command/ViewProblemSetsCommand.java
@@ -0,0 +1,27 @@
+package command;
+
+import databaseAPI.DatabaseAPI;
+import io.OutputGen;
+
+public class ViewProblemSetsCommand extends Command {
+    /**
+     * Creates a new command using the specified database API method and output generator.
+     *
+     * @param databaseAPI  the database API instance to use with the action
+     * @param outputStream the output generator to use with the command
+     */
+    public ViewProblemSetsCommand(DatabaseAPI databaseAPI, OutputGen outputStream) {
+        super(databaseAPI, outputStream);
+    }
+
+    /**
+     * Executes the given command with the provided arguments. Any outputs created are the command's output generator.
+     *
+     * @param args the arguments for the command to use. No arguments are used in this command.
+     * @return whether or not the command succeeded
+     */
+    @Override
+    public boolean execute(String[] args) {
+        return false;
+    }
+}
