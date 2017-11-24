259328ce12e089d71bc747e6e88514170b5c2a66
Samuel Zhou <samuel-zhou@outlook.com>
2017-11-23 20:27:21 -0500

    Remove 'do fake problem set' button

--- JWorks/src/gui/StudentInnerScreenManager.java
+++ JWorks/src/gui/StudentInnerScreenManager.java
@@ -20,16 +20,6 @@ public class StudentInnerScreenManager extends Manager{
     controller.start(this);
   }
 
-  /**
-   * Creates a new instance of the complete problem set screen, with the given problem set.
-   * @param innerPane The inner screen to load onto
-   * @param problemSet the problem set to attempt
-   */
-  public void attemptProblemSet(Pane innerPane, ProblemSet problemSet) {
-    CompleteProblemSetScreenManager manager = new CompleteProblemSetScreenManager();
-    manager.showScreen(innerPane, problemSet);
-  }
-
   public void viewProblemSets(Pane innerPane) {
     ViewProblemSetScreenManager manager = new ViewProblemSetScreenManager();
     manager.showScreen(innerPane);
