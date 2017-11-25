fa57e8e6a18c5c78555c650b4c0d52109a020df7
James Gabriel Goudie <james.goudie@mail.utoronto.ca>
2017-11-25 02:03:00 -0500

    Added functionality to retrieve all of the data from the database regarding problem sets through the API.

--- JWorks/src/databaseAPI/DatabaseExtractAPI.java
+++ JWorks/src/databaseAPI/DatabaseExtractAPI.java
@@ -447,9 +447,41 @@ public class DatabaseExtractAPI extends DatabaseSelector implements DatabaseAPI{
                         previousAttemptData.getString(2));
             }
         } catch (DatabaseSelectException | SQLException e) {
-          
+            e.printStackTrace();
         }
         
         return problemSetAttempt;
+    }
+
+    /**
+     * 
+     * @return A list containing all of the problem set attempts in the database. An empty list
+     *         indicates that an error may have occurred.
+     */
+    public List<ProblemSetAttempt> getAllAttempts() {
+        this.actOnDatabase();
+        List<ProblemSetAttempt> allAttempts = new ArrayList<>();
+        
+        try {
+            ResultSet allAttemptData = DatabaseSelector.getAllAttemptIdentifiers(this.connection);
+            
+            while(allAttemptData.next()) {
+                int studentNumber = allAttemptData.getInt(1);
+                int problemSetID = allAttemptData.getInt(2);
+                Date attemptTime = new Date(allAttemptData.getInt(3) * 1000);
+                    
+                Student student = this.actOnDatabase(studentNumber, new Student());
+                ProblemSet problemSet = this.actOnDatabase(problemSetID, new SimpleProblemSet());
+                
+                ProblemSetAttempt newAttempt = new ProblemSetAttempt(student, problemSet,
+                        attemptTime);
+                
+                allAttempts.add(this.actOnDatabase(newAttempt));
+            }
+        } catch (DatabaseSelectException | SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return allAttempts;
     }
 }
