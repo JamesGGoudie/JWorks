ceaed6bc36a8fddbb2a256c924065bdb29555f0d
Samuel Zhou <samuel-zhou@outlook.com>
2017-11-26 15:31:24 -0500

    Implement bulk problem adding

--- JWorks/src/gui/CreateNewQuestionScreenManager.java
+++ JWorks/src/gui/CreateNewQuestionScreenManager.java
@@ -1,6 +1,11 @@
 package gui;
 
+import io.FileParser;
 import javafx.scene.layout.Pane;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class CreateNewQuestionScreenManager extends Manager {
 	private final String COMMAND = "AddSimpleProblemCommand";
@@ -15,7 +20,30 @@ public class CreateNewQuestionScreenManager extends Manager {
 	public void createNewQuestion(String question, String answer, String tagString) {
 		inputValues = new String[] { COMMAND, question, answer, tagString };
 		// execute the action in interpreter
-		interpreter.executeAction(inputValues);
+		final boolean success = interpreter.executeAction(inputValues);
+
+		// inform user of command result
+		if (success) {
+			interpreter.getOutputGenerator().output("Problem successfully added!");
+		} else {
+			interpreter.getOutputGenerator().output("Something went wrong!");
+		}
+	}
+
+	/**
+	 * Imports problems from the given file.
+	 * @param file the file to import problems from.
+	 */
+	public void createBulkProblems(File file) {
+		try {
+			List<String[]> problems = FileParser.generateArgStringFromFile(file, COMMAND);
+			for (String[] problemCommands : problems) {
+				interpreter.executeAction(problemCommands);
+			}
+			interpreter.getOutputGenerator().output("Problems successfully imported!");
+		} catch (IOException e) {
+			interpreter.getOutputGenerator().output("Something went wrong! Check the format of your file");
+		}
 	}
 
 	/**
