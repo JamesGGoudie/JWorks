21698375bfcb65f317fc385d18f894add283fef9
Samuel Zhou <samuel-zhou@outlook.com>
2017-11-25 20:15:30 -0500

    Refactor common tag components to new controller

--- JWorks/src/gui/ViewProblemsController.java
+++ JWorks/src/gui/ViewProblemsController.java
@@ -7,14 +7,13 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
 import models.Problem;
 
 import java.util.List;
 
-public class ViewProblemsController extends Controller {
+public class ViewProblemsController extends WrappableViewController<Problem> {
     @FXML
     public TableView<Problem> questionTable;
 
@@ -26,18 +25,6 @@ public class ViewProblemsController extends Controller {
 
     @FXML
     private TableColumn<Problem, String> answerColumn;
-
-    @FXML
-    private TextField filterField;
-
-    @FXML
-    private Button filterButton;
-
-    @FXML
-    private Button clearButton;
-
-    @FXML
-    private TableColumn<Problem, String> tagsColumn;
 
     private ViewProblemsManager manager;
 
@@ -60,49 +47,20 @@ public class ViewProblemsController extends Controller {
         // Wrap text
         wrapColumnCells(questionColumn);
         wrapColumnCells(answerColumn);
-        wrapColumnCells(tagsColumn);
-
-        tagsColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Problem, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Problem, String> param) {
-                String tagString = "";
-                List<String> tags = param.getValue().getTags();
-
-                for (String tag : tags) {
-                    tagString = tagString + tag + "; ";
-                }
-
-                return new ReadOnlyObjectWrapper<>(tagString);
-            }
-        });
 
         // Setup tag filters
         clearButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                filterField.clear();
+                searchField.clear();
                 questionTable.getItems().setAll(manager.getProblems());
             }
         });
 
-        filterButton.setOnAction(new EventHandler<ActionEvent>() {
+        searchButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                questionTable.getItems().setAll(manager.getProblems(filterField.getText()));
-            }
-        });
-    }
-
-    private void wrapColumnCells(TableColumn<Problem, String> column) {
-        column.setCellFactory(new Callback<TableColumn<Problem, String>, TableCell<Problem, String>>() {
-            @Override
-            public TableCell<Problem, String> call(TableColumn<Problem, String> param) {
-                TableCell<Problem, String> cell = new TableCell<>();
-                Text value = new Text();
-                cell.setGraphic(value);
-                value.wrappingWidthProperty().bind(cell.widthProperty());
-                value.textProperty().bind(cell.itemProperty());
-                return cell;
+                questionTable.getItems().setAll(manager.getProblems(searchField.getText()));
             }
         });
     }
--- JWorks/src/gui/WrappableViewController.java
+++ JWorks/src/gui/WrappableViewController.java
@@ -1,4 +1,68 @@
 package gui;
 
-public class WrappableViewController {
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
+import models.Taggable;
+
+import java.util.List;
+
+public class WrappableViewController<T extends Taggable> extends Controller{
+
+    @FXML
+    protected TextField searchField;
+
+    @FXML
+    protected Button searchButton;
+
+    @FXML
+    protected Button clearButton;
+
+    @FXML
+    protected TableColumn<T, String> tagsColumn;
+
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        tagsColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<T, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<T, String> param) {
+                String tagString = "";
+                List<String> tags = param.getValue().getTags();
+
+                for (String tag : tags) {
+                    tagString = tagString + tag + "; ";
+                }
+
+                return new ReadOnlyObjectWrapper<>(tagString);
+            }
+        });
+
+        wrapColumnCells(tagsColumn);
+    }
+
+    /**
+     * Sets the cell factory for the given string column such that the text is wrapped.
+     * @param column the column to wrap
+     */
+    protected void wrapColumnCells(TableColumn<T, String> column) {
+        column.setCellFactory(new Callback<TableColumn<T, String>, TableCell<T, String>>() {
+            @Override
+            public TableCell<T, String> call(TableColumn<T, String> param) {
+                TableCell<T, String> cell = new TableCell<>();
+                Text value = new Text();
+                cell.setGraphic(value);
+                value.wrappingWidthProperty().bind(cell.widthProperty());
+                value.textProperty().bind(cell.itemProperty());
+                return cell;
+            }
+        });
+    }
 }
