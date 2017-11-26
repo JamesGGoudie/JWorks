7ce266e79ae8e586b51d9d7d1c2d0f045280aeb0
Samuel Zhou <samuel-zhou@outlook.com>
2017-11-26 15:05:12 -0500

    Implement bulk student import

--- JWorks/src/gui/CreateNewStudentAccountManager.java
+++ JWorks/src/gui/CreateNewStudentAccountManager.java
@@ -1,6 +1,11 @@
 package gui;
 
+import io.FileParser;
 import javafx.scene.layout.Pane;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class CreateNewStudentAccountManager extends Manager {
 	private CreateNewStudentAccountController controller;
@@ -16,8 +21,25 @@ public class CreateNewStudentAccountManager extends Manager {
 	}
 
 	public void createStudentAccount(String name, String email, String password, String studentNumber) {
-		inputValues = new String[] {COMMAND, name, email, password, studentNumber};
+		inputValues = new String[] {COMMAND, name, email, password, studentNumber, ""};
 		interpreter.executeAction(inputValues);
+	}
+
+	/**
+	 * Creates student accounts in bulk by reading the given file line by line and parsing the arguments.
+	 * @param file the file to read
+	 */
+	public void createBulkStudentAccounts(File file) {
+		try {
+			List<String[]> students = FileParser.generateArgStringFromFile(file, COMMAND);
+			for (String[] studentCommand : students) {
+				interpreter.executeAction(studentCommand);
+			}
+			interpreter.getOutputGenerator().output("Students successfully imported!");
+		} catch (IOException e) {
+			// Show some error
+			interpreter.getOutputGenerator().output("Something went wrong! Check the format of your file");
+		}
 	}
 	
 
